ac5d46aa047dc8e13e99b10b897bbf734cf3f230
Pranav <pranavmendi@gmail.com>
2021-07-05 20:57:56 +0530

    Add functionality to Sign-up a user using Firebase Auth API

--- src/components/Auth/AuthForm.js
+++ src/components/Auth/AuthForm.js
@@ -1,39 +1,99 @@
-import { useState } from 'react';
-
+import { useRef, useState } from 'react';
 import classes from './AuthForm.module.css';
+require('dotenv').config();
 
 const AuthForm = () => {
-  const [isLogin, setIsLogin] = useState(true);
+	const [isLogin, setIsLogin] = useState(true);
+	const [isLoading, setIsLoading] = useState(false);
 
-  const switchAuthModeHandler = () => {
-    setIsLogin((prevState) => !prevState);
-  };
+	const emailInputRef = useRef();
+	const passwordInputRef = useRef();
 
-  return (
-    <section className={classes.auth}>
-      <h1>{isLogin ? 'Login' : 'Sign Up'}</h1>
-      <form>
-        <div className={classes.control}>
-          <label htmlFor='email'>Your Email</label>
-          <input type='email' id='email' required />
-        </div>
-        <div className={classes.control}>
-          <label htmlFor='password'>Your Password</label>
-          <input type='password' id='password' required />
-        </div>
-        <div className={classes.actions}>
-          <button>{isLogin ? 'Login' : 'Create Account'}</button>
-          <button
-            type='button'
-            className={classes.toggle}
-            onClick={switchAuthModeHandler}
-          >
-            {isLogin ? 'Create new account' : 'Login with existing account'}
-          </button>
-        </div>
-      </form>
-    </section>
-  );
+	const switchAuthModeHandler = () => {
+		setIsLogin((prevState) => !prevState);
+	};
+
+	const submitHandler = (e) => {
+		e.preventDefault();
+
+		const enteredEmail = emailInputRef.current.value;
+		const enteredPassword = passwordInputRef.current.value;
+		//validation
+
+		setIsLoading(true);
+		if (isLogin) {
+		} else {
+			fetch(
+				`https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${process.env.REACT_APP_WEB_KEY}`,
+				{
+					method: 'POST',
+					body: JSON.stringify({
+						email: enteredEmail,
+						password: enteredPassword,
+						returnSecureToken: true,
+					}),
+					headers: {
+						'content-type': 'application/json',
+					},
+				}
+			)
+				.then((res) => {
+					setIsLoading(false);
+					if (res.ok) {
+					} else {
+						res.json().then((data) => {
+							console.log(data);
+							let errorMessage = 'Authentication Failed!';
+							if (data && data.error) {
+								errorMessage = data.error.message;
+							}
+							alert(errorMessage);
+						});
+					}
+				})
+				.catch();
+		}
+	};
+
+	return (
+		<section className={classes.auth}>
+			<h1>{isLogin ? 'Login' : 'Sign Up'}</h1>
+			<form onSubmit={submitHandler}>
+				<div className={classes.control}>
+					<label htmlFor='email'>Your Email</label>
+					<input
+						type='email'
+						id='email'
+						ref={emailInputRef}
+						required
+					/>
+				</div>
+				<div className={classes.control}>
+					<label htmlFor='password'>Your Password</label>
+					<input
+						type='password'
+						id='password'
+						ref={passwordInputRef}
+						required
+					/>
+				</div>
+				<div className={classes.actions}>
+					{!isLoading && (
+						<button>{isLogin ? 'Login' : 'Create Account'}</button>
+					)}
+					{isLoading && <p>Sending Request..</p>}
+					<button
+						type='button'
+						className={classes.toggle}
+						onClick={switchAuthModeHandler}>
+						{isLogin
+							? 'Create new account'
+							: 'Login with existing account'}
+					</button>
+				</div>
+			</form>
+		</section>
+	);
 };
 
 export default AuthForm;
